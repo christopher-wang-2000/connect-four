7944dc81b0ee3bbbdcffb95009b852ac6faa71b5
Chris Wang <chrisw.msf@gmail.com>
2021-10-13 00:17:43 -0500

    Increased CPU difficulty

--- connect-four/src/connectfour/Opponent.java
+++ connect-four/src/connectfour/Opponent.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class Opponent {
 	
+	static final int noGoodMoves = -2; // findGoodMoves return value
+	static final int hasGoodMoves = -1; // findGoodMoves return value
+	
 	static int play(Board b, char symbol) {
 		
 		Random r = new Random();
@@ -17,14 +20,22 @@ public class Opponent {
 		
 		// otherwise, check if any moves will prevent the player from winning
 		ArrayList<Integer> goodMoves = new ArrayList<>();		
-		if (findGoodMoves(b, symbol, ConnectFour.p1, goodMoves)) {
+		int move = findGoodMoves(b, symbol, ConnectFour.p1, goodMoves, 1);
+		
+		// if there is a move that will force a win on your next turn, do it
+		if (move >= 0) {
+			b.addPiece(move, symbol);
+			return move;
+		}
+		
+		if (move == hasGoodMoves) {
 			// pick a random column of the available good moves
 			col = goodMoves.get(r.nextInt(goodMoves.size()));
 			b.addPiece(col, symbol);
 			return col;
 		}
 		
-		// otherwise, pick a random column
+		// otherwise, there are no good moves, so pick a random column
 		col = r.nextInt(b.width);
 		
 		// try adding the piece; if it works, return the column
@@ -56,23 +67,46 @@ public class Opponent {
 		return -1; // if no winning move is found
 	}
 	
-	// searches for moves that won't let the player win next turn
-	// returns true if there are any, false if there aren't
-	static boolean findGoodMoves(Board b, char symbol, char other, ArrayList<Integer> goodMoves) {
+	// searches for moves that don't let the player win next turn
+	// if a move doesn't lead to a loss, add it to the goodMoves list
+	//
+	// if a move forces a win on your next turn, return it immediately
+	// otherwise, return hasGoodMoves if there are some moves in goodMoves
+	// and return noGoodMoves if there aren't
+	//
+	// iterate keeps track of whether this is the first call or the recursive call
+	static int findGoodMoves(Board b, char symbol, char other,
+			ArrayList<Integer> goodMoves, int iterate) {
 		
 		for (int i = 0; i < b.width; i++) {
 			
 			// temporarily add a piece
 			if (b.addPiece(i, symbol)) {
+				
 				// if the player cannot instantly win, it is a good move
 				if(searchVictory(b, other) == -1) {
 					goodMoves.add(i);
+					
+					// check if there are good moves on the player's side
+					// if not, this move forces a win, so return immediately
+					if (iterate > 0) {
+						ArrayList<Integer> dummy = new ArrayList<>();
+						if (findGoodMoves(b, other, symbol, dummy, iterate - 1) == noGoodMoves) {
+							System.out.println("hello!");
+							return i;
+						}
+					}
+					
 				}
+				
 				b.removePiece(i); // remove the temporary piece
 			}
 			
 		}
-		return goodMoves.size() > 0;
+		if (goodMoves.size() > 0) {
+			return hasGoodMoves;
+		}
+		return noGoodMoves;
 	}
 	
 	
